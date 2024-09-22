22d26e4ad262a5bb6117cd63b9525d9d86e4df5d
jh080724 <tsj010813@gmail.com>
2024-09-22 21:53:39 +0900

    29Line: sum 초기값을 0->1로 수정함.

--- src/array/Solution2018.java
+++ src/array/Solution2018.java
@@ -10,6 +10,12 @@ public class Solution2018 {
     public static void main(String[] args) {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
+        /*
+        https://www.acmicpc.net/problem/2018
+        입력: 15, 출력: 4
+        * */
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] arr = new int[N];
@@ -20,7 +26,7 @@ public class Solution2018 {
 
 //        System.out.println(Arrays.toString(arr));
 
-        int start = 0, end = 0, count = 0, sum = 0;
+        int start = 0, end = 0, count = 0, sum = 1;
 
 //        while(end < arr.length){
         while(end < N){
@@ -34,7 +40,7 @@ public class Solution2018 {
                 start++;
             } else{
                 end++;
-                sum+=arr[end];  // 터졌음. 확인할 것.
+                sum+=arr[end];  // 터졌음. 확인할 것. --> 29Line: sum 초기값을 0->1로 수정함.
             }
 
         }
