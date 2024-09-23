7d2c5bf586004da8559dcea851751403307a9959
jh080724 <tsj010813@gmail.com>
2024-09-23 17:49:47 +0900

    Queue 실습(숙제-코드 작성전)

---
+++ src/queue/Queue1.java
@@ -0,0 +1,7 @@
+package queue;
+
+public class Queue1 {
+    // 백준 2164와 동일한 개념
+    // 2장 붙이고 한장 버리고
+    // 스택과규 문제5.
+}
---
+++ src/queue/Queue2.java
@@ -0,0 +1,7 @@
+package queue;
+
+public class Queue2 {
+    public static void main(String[] args) {
+        // 스택과규 문제6.
+    }
+}
