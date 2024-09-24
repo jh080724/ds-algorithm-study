f4a46e0fa8338ee99b730324f6e7277ce14c6e6c
jh080724 <tsj010813@gmail.com>
2024-09-24 17:49:28 +0900

    Recursive1

---
+++ src/recursive/Recursive1.java
@@ -0,0 +1,36 @@
+package recursive;
+
+import java.util.Scanner;
+
+public class Recursive1 {
+    static int N;
+    static int[] check;
+
+    static void DFS(int L) {
+        if(L == N + 1){
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= N; i++) {
+                if(check[i]==1){
+                    sb.append(i + " ");
+                }
+            }
+
+            if(!sb.isEmpty()) System.out.println(sb);
+
+        } else{
+            check[L] = 1;
+            DFS(L + 1);  // LT
+            check[L] = 0;
+            DFS(L + 1); // RT
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        check = new int[N + 1];
+        DFS(1);
+    }
+}
